e552c9ecbc76d994012f96e97112a657515d0b02
Ben Morin <bamorin00@gmail.com>
2020-07-20 01:51:52 +0000

    basic docs

--- README.md
+++ README.md
@@ -1,6 +1,91 @@
 [![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/BeAnMo/check-mate)
 
-# Checkest
+# checkest
 
 ##### Simple, composable, dependency free data validator.
 
+The goal of checkest is to provide a uniform way of validating data across JavaScript environments. The basic API was inspired by Meteor's [check](https://docs.meteor.com/api/check.html) package, along with a few ideas pulled from [prop-types](https://github.com/facebook/prop-types).
+
+
+### Installation
+
+PENDING
+
+### API
+
+`any, any => { passed: boolean, error?: string }`
+- check(<data>, <shape>)
+
+
+##### Basic predicates
+
+`any => boolean`
+- isNull
+- isUndefined
+- isBoolean
+    - Strict check for true/false
+- isNumber
+    - Returns true for integers/floats/BigInts/MAX & MIN_VALUES/Math values (PI, etc...)
+- isString
+- isArray (alias for Array.isArray)
+- isObject
+    - Returns true if input is not and Array/null and is typeof 'object'
+- isFunction
+
+##### Additional
+
+`RegExp => string => boolean`
+- regexpMatch
+
+`...(any => boolean) => any => boolean`
+- isOneOf
+    - `isOneOf(isString, isNumber)('hi') // true`
+    - `isOneOf(isString, isNumber)(BigInt('343434636537573683565635')) // true`
+    - `isOneOf(isString, isNumber)(true) // false`
+
+`...any => any => boolean`
+- isOneOfValues
+    - `isOneOfValues('a', 1, null)(4) // false`
+    - `isOneOfValues('a', 1, null)(1) // true`
+
+`(any => boolean) => any => boolean`
+- isNonEmpty
+    - `isNonEmpty(arr => arr[1] === 2)([1,2,3]) // true`
+    - `isNonEmpty(arr => arr[2] === 2)([]) // false`
+
+`(any => boolean) => any => boolean`
+- maybe
+    - `maybe(isString)('hello') // true`
+    - `maybe(isString)() // true`
+    - `maybe(isString)(1010) // false`
+
+### Examples
+
+*As Express middleware*
+```js
+import { check, isString, isNumber, isNonEmpty, regexpMatch } from 'checkest';
+
+const USER_FORM = {
+    email: regexpMatch(EMAIL_RX),
+    password: isNonEmpty(isString),
+    optional: maybe({
+        age: isNumber,
+        city: isString,
+        serialNumber: isNumber
+    })
+};
+
+const userValidation = (req, res, next)=> {
+    const { passed, error } = check(req.body, USER_FORM);
+
+    if(!passed){
+        res.json({ error });
+    } else {
+        next();
+    }
+}
+
+router.post('/signup', userValidation, (req, res) => {
+    // ...
+});
+```
--- src/predicates.js
+++ src/predicates.js
@@ -1,80 +1,80 @@
 //@ts-check
 
 export function isString(x) {
-  return typeof x === 'string';
+    return typeof x === 'string';
 }
 
 export function isNumber(x) {
-  const type = typeof x;
+    const type = typeof x;
 
-  if (type === 'bigint') {
-    return true;
-  } else {
-    return type === 'number' && !isNaN(x);
-  }
+    if (type === 'bigint') {
+        return true;
+    } else {
+        return type === 'number' && !isNaN(x);
+    }
 }
 
 export function isBoolean(x) {
-  return typeof x === 'boolean';
+    return typeof x === 'boolean';
 }
 
 export function isNull(x) {
-  return x === null;
+    return x === null;
 }
 
 export function isUndefined(x) {
-  return x === undefined;
+    return x === undefined;
 }
 
 export function isAtom(x) {
-  return (
-    isString(x) || isNumber(x) || isBoolean(x) || isNull(x) || isUndefined(x)
-  );
+    return (
+        isString(x) || isNumber(x) || isBoolean(x) || isNull(x) || isUndefined(x)
+    );
 }
 
 export function isArray(x) {
-  return Array.isArray(x);
+    return Array.isArray(x);
 }
 
 export function isObject(x) {
-  return !isArray(x) && typeof x === 'object';
+    return !isArray(x) && !isNull(x) && typeof x === 'object';
 }
 
 export function isFunction(x) {
-  return typeof x === 'function';
+    return typeof x === 'function';
 }
 
 export function regexpMatch(pattern) {
-  return (x) => pattern.test(x);
+    return (x) => pattern.test(x);
 }
 
 export function maybe(predicate) {
-  return (x) => predicate(x) || true;
+    return (x) => predicate(x) || true;
 }
 
 export function isOneOfValues(...vals) {
-  return (x) => vals.some((y) => y === x);
+    return (x) => vals.some((y) => y === x);
 }
 
 export function isOneOf(...predicates) {
-  return (x) => predicates.some((p) => p(x));
+    return (x) => predicates.some((p) => p(x));
 }
 
 export function isNonEmpty(predicate) {
-  return (x) => {
-    switch (typeof x) {
-      case 'string':
-        return x.length > 0 && predicate(x);
+    return (x) => {
+        switch (typeof x) {
+            case 'string':
+                return x.length > 0 && predicate(x);
 
-      case 'object':
-        if (isArray(x)) {
-          return x.length > 0 && predicate(x);
-        } else {
-          return Object.keys(x).length > 0 && predicate(x);
+            case 'object':
+                if (isArray(x)) {
+                    return x.length > 0 && predicate(x);
+                } else {
+                    return Object.keys(x).length > 0 && predicate(x);
+                }
+
+            default:
+                return predicate(x);
         }
-
-      default:
-        return predicate(x);
-    }
-  };
+    };
 }
